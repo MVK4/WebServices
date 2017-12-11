713b1746c568a0b68cb588779480479046e1938d
mvk <mvk@10.0.0.126>
2017-12-10 16:18:47 -0800

    WebServices Commit and push

---
+++ com.test.apiTest/src/test/java/apiTesting/com/test/apiTest/WheatherGetRequest.java
@@ -0,0 +1,40 @@
+package apiTesting.com.test.apiTest;
+
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Response;
+
+import static com.jayway.restassured.RestAssured.*;
+
+public class WheatherGetRequest {
+
+	@Test
+	public void Test_01() {
+		
+		Response resp1 = when().
+						get("http://samples.openweathermap.org/data/2.5/weather?q=London&appid=4417e95814ce6fed700344534efbf944");
+		
+		System.out.println(resp1.statusCode());
+	}
+	
+	@Test
+	public void Test_02() {
+		
+		Response resp2 = when().
+						get("http://samples.openweathermap.org/data/2.5/weather?q=London&appid=4417e95814ce6fed700344534efbf944");
+		
+		System.out.println(resp2.statusCode());
+	}
+	
+	@Test
+	public void Test_03() {
+		
+		Response resp2 = given().
+						param("q", "London").
+						param("appid","4417e95814ce6fed700344534efbf944").	
+						when().
+						get("http://samples.openweathermap.org/data/2.5/weather");
+		
+		System.out.println(resp2.statusCode());
+	}
+}
